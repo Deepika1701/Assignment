b89a850629f320478eb4c73642b89ee4870ad530
Deepika1701 <91446733+Deepika1701@users.noreply.github.com>
2021-12-01 10:10:35 +0530

    Delete assignment/assignment-Day-6 directory

--- assignment/assignment-Day-6/ForLoopPrograms/factorial.sh
+++
@@ -1,8 +0,0 @@
-read -p "enter number :-" num
-p=1
-for((i=1;i<=$num;i++))
-do
-    p=$((p*i))
-done
-
-echo "factorial of "$num "=" $p
--- assignment/assignment-Day-6/ForLoopPrograms/powersOf2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-echo "Table of powers of 2 is : "
-for((num=0;num<=$1;num++))
-do
-        echo $((2**num))
-done
-
